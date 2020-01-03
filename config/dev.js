54b91549815683146b3151f8c13331bd616ef3d1
liulinboyi <814921718@qq.com>
2020-01-03 23:49:28 +0800

    feat: 添加h5版本跨域

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,22 @@
 module.exports = {
   env: {
-    NODE_ENV: '"development"'
+    NODE_ENV: '"development"',
+    CLIENT_ENV: process.env.CLIENT_ENV,
   },
   defineConstants: {
   },
   weapp: {},
-  h5: {}
+  h5: {
+    devServer: {
+      // 同一局域网访问
+      // host: "0.0.0.0",
+      port: 10086,
+      proxy: {
+        '/api': {
+          target: "https://www.v2ex.com",
+          changeOrigin: true
+        }
+      }
+    }
+  }
 }
